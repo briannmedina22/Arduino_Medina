d5e111945d1d008b17cf0755a40b991a567ed75a
briannmedina22 <42305742+briannmedina22@users.noreply.github.com>
2018-09-19 09:00:49 -0300

    Add files via upload

---
+++ trabajopracticomedina/Arduino_Briannmedina22.ino
@@ -0,0 +1,27 @@
+#include <EEPROM.h>
+int direccion=0;
+byte val1;
+byte val2;
+byte Dec;
+
+void setup (){
+  Serial.begin (9600);
+  byte informacion = B11001;
+  int valor = analogRead (0);
+  EEPROM.write(direccion, informacion);
+  EEPROM.write(direccion+1,valor);}
+
+void loop() {
+  EEPROM.read(direccion);
+  EEPROM.read(direccion+1);
+  Serial.print("En la direccion esta;");
+  Serial.print(direccion);
+  Serial.print("Se encuentra");
+  Serial.print(val1,Dec);
+  delay(100);
+  Serial.print("En la direccion esta:");
+  Serial.print(direccion+1);
+  Serial.print("Se encuentra");
+  Serial.print(val2,Dec);
+  delay(100);
+}
